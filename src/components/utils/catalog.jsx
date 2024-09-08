2baa1de2b1cb6f13534281d61f401f435922bf16
Talles Coelho <talescoelho43@gmail.com>
2024-09-08 11:03:28 -0300

    feat: criado arquivo de catalogo

---
+++ src/components/utils/catalog.jsx
@@ -0,0 +1,75 @@
+import product1Img from "../assets/img/product-1.jpg";
+import product2Img from "../assets/img/product-2.jpg";
+import product3Img from "../assets/img/product-3.jpg";
+import product4Img from "../assets/img/product-4.jpg";
+import product5Img from "../assets/img/product-5.jpg";
+import product6Img from "../assets/img/product-6.jpg";
+import product7Img from "../assets/img/product-7.jpg";
+import product8Img from "../assets/img/product-8.jpg";
+
+export const catalog = [
+	{
+		id: 1,
+		brand: "Zara",
+		name: "Camisa Larga com Bolsos",
+		price: 70,
+		image: product1Img,
+		feminine: false,
+	},
+	{
+		id: 2,
+		brand: "Zara",
+		name: "Casaco Reto com Lã",
+		price: 85,
+		image: product2Img,
+		feminine: true,
+	},
+	{
+		id: 3,
+		brand: "Zara",
+		name: "Jaqueta com Efeito Camurça",
+		price: 60,
+		image: product3Img,
+		feminine: false,
+	},
+	{
+		id: 4,
+		brand: "Zara",
+		name: "Sobretudo em Mescla de Lã",
+		price: 160,
+		image: product4Img,
+		feminine: false,
+	},
+	{
+		id: 5,
+		brand: "Zara",
+		name: "Camisa Larga Acolchoada de Veludo Cotelê",
+		price: 110,
+		image: product5Img,
+		feminine: false,
+	},
+	{
+		id: 6,
+		brand: "Zara",
+		name: "Casaco de Lã com Botões",
+		price: 170,
+		image: product6Img,
+		feminine: true,
+	},
+	{
+		id: 7,
+		brand: "Zara",
+		name: "Casaco com Botões",
+		price: 75,
+		image: product7Img,
+		feminine: true,
+	},
+	{
+		id: 8,
+		brand: "Zara",
+		name: "Colete Comprido com Cinto",
+		price: 88,
+		image: product8Img,
+		feminine: true,
+	},
+];
